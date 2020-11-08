9b0686d8a84f4b6a02ad97e500083269e0da5228
harounaKane <harouna.kane@hotmail.fr>
2020-11-08 20:04:53 +0100

    creation d'un router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,28 @@
+import { Routes, RouterModule } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainComponent } from './main/main.component';
+import { UserComponent } from './user/user.component';
+
+// création de route
+const routes: Routes = [
+  {path: "user", component: UserComponent},
+  {path: "", component: MainComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainComponent
+    MainComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    //AppRoutingModule "devient"
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
